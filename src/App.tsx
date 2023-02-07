a061b59df40b2b8f9e2062e8f194f7f4e088e0c6
te6-in <56245920+te6-in@users.noreply.github.com>
2023-02-07 21:35:36 +0900

    Calculate sum of durations by summary (except recurring events)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,49 @@
 import * as ICAL from "ical.js";
 import { ChangeEvent, useEffect, useState } from "react";
 
+class Event {
+	readonly durationMilliseconds: number;
+
+	constructor(
+		readonly start: Date,
+		readonly end: Date,
+		readonly recurRules: any, // TODO
+		readonly excludedDates: Date[] | null
+	) {
+		if (recurRules === null) {
+			this.durationMilliseconds = this.end.getTime() - this.start.getTime();
+		} else {
+			this.durationMilliseconds = 0; // TODO
+		}
+	}
+}
+
+class Events {
+	durationMilliseconds: number;
+
+	constructor(readonly summary: string, readonly events: Event[]) {
+		this.durationMilliseconds = 0;
+	}
+
+	setDurationMilliseconds() {
+		this.durationMilliseconds = this.events.reduce(
+			(accumulator, event) => accumulator + event.durationMilliseconds,
+			0
+		);
+	}
+}
+
+function getDate(data: ICAL.Time) {
+	return new Date(
+		data.year,
+		data.month - 1,
+		data.day,
+		data.hour,
+		data.minute,
+		data.second
+	);
+}
+
 function App() {
 	const [icalData, setCalendarData] = useState<string | null>(null);
 
@@ -27,6 +70,8 @@ function App() {
 		}
 	};
 
+	const calendar: Events[] = [];
+
 	useEffect(() => {
 		if (icalData) {
 			const jcalData = ICAL.parse(icalData);
@@ -34,14 +79,38 @@ function App() {
 			const events = componentData.getAllSubcomponents("vevent");
 
 			events.forEach((event) => {
-				if (event.getFirstPropertyValue("summary") === "학원") {
-					console.log(
-						event,
-						event.getFirstPropertyValue("dtstart"),
-						event.getFirstPropertyValue("dtend")
-					);
+				const summary = event.getFirstPropertyValue("summary");
+				const excludedDates =
+					event.getAllProperties("exdate").length === 0
+						? null
+						: event.getAllProperties("exdate").map((exdate) => {
+								const excludeData = exdate.getFirstValue()._time as ICAL.Time;
+								return getDate(excludeData);
+						  });
+
+				const newEvent = new Event(
+					getDate(event.getFirstPropertyValue("dtstart")._time),
+					getDate(event.getFirstPropertyValue("dtend")._time),
+					event.getFirstPropertyValue("rrule"),
+					excludedDates
+				);
+
+				const index = calendar.findIndex(
+					(calendarEvent) => calendarEvent.summary === summary
+				);
+
+				if (index === -1) {
+					calendar.push(new Events(summary, [newEvent]));
+				} else {
+					calendar[index].events.push(newEvent);
 				}
 			});
+
+			calendar.forEach((events) => {
+				events.setDurationMilliseconds();
+			});
+
+			console.log(calendar);
 		}
 	}, [icalData]);
 
