0e993a6a5d6deea4b7d61e177046466bf7c24000
Chanhwi Joo <56245920+te6-in@users.noreply.github.com>
2023-09-11 17:41:56 +0900

    Put script in head

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,17 @@ import App from "@/components/App";
 import "@/index.pcss";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Helmet } from "react-helmet";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
+		<Helmet>
+			<script
+				async
+				src="https://analytics.umami.is/script.js"
+				data-website-id="d5f6f31c-f01f-4a86-942b-2f4bff151e15"
+			/>
+		</Helmet>
 		<App />
-		<script
-			async
-			src="https://analytics.umami.is/script.js"
-			data-website-id="d5f6f31c-f01f-4a86-942b-2f4bff151e15"
-		/>
 	</React.StrictMode>
 );
